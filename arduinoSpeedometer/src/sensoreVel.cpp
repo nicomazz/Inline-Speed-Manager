e11287f5f13534727484a4f1ff68e1e9159c18ff
Nicolò Mazzucato <nicomazz@users.noreply.github.com>
2017-04-28 21:47:41 +0200

    Code refactor

--- arduinoSpeedometer/src/sensoreVel.cpp
+++ arduinoSpeedometer/src/sensoreVel.cpp
@@ -15,6 +15,7 @@ void printMillis(){
    mySerial.print(millis());
    mySerial.print("|");
 }
+
 #ifdef TEST_ENABLED
 void test(){
    int millisecond = random(0, 1000*20); // from 0 to 20 sec
@@ -26,9 +27,8 @@ void test(){
 void setup(){
    Serial.begin(9600);
    Serial.println("inizio!");
-
+  
    pinMode(LASER_INPUT, INPUT);
-
    pinMode(LASER_VCC,OUTPUT);
    digitalWrite(LASER_VCC,HIGH);
    pinMode(LASER_GND,OUTPUT);
@@ -37,31 +37,28 @@ void setup(){
    mySerial.begin(9600);
    Serial.println("ARDUINO SPEEDOMETER started");
    mySerial.println("ARDUINO SPEEDOMETER started");
-
-
-
 }
 
 inline bool laserInterruped(){
    return !digitalRead(LASER_INPUT);
 }
+
 void checkForStartMessages(){
    if (mySerial.available()){
       int inByte = mySerial.read();
       printMillis();
    }
 }
-bool laserInterrupedState = laserInterruped();
-void loop() {
 
+bool laserInterrupedState = laserInterruped();
+
+void loop() {
 #ifdef TEST_ENABLED
    test();
 #else
-
    digitalWrite(LED_PIN,laserInterrupedState);
    while (laserInterruped() == laserInterrupedState) checkForStartMessages();
    laserInterrupedState = !laserInterrupedState;
    printMillis();
-
 #endif
 }
